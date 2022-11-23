5a275dc5deea172f3b3d60aa7a6b985fdd97e9db
rcaa <rodrigo.andrade@ufape.edu.br>
2022-11-23 14:49:33 -0300

    projeto para treinar refactorings

---
+++ src/primeiroRefactoring/Pessoa.java
@@ -0,0 +1,84 @@
+package primeiroRefactoring;
+
+import java.util.Date;
+
+public class Pessoa {
+
+	private String nome;
+	private String cpf;
+	private String registroGeral;
+	private Date dataNascimento;
+	private String nomePai;
+	private String nomeMae;
+	private String logradouro;
+	private String complemento;
+	private String cep;
+	
+	public Pessoa(String nome, String cpf, String registroGeral, Date dataNascimento, String nomePai, String nomeMae,
+			String logradouro, String complemento, String cep) {
+		super();
+		this.nome = nome;
+		this.cpf = cpf;
+		this.registroGeral = registroGeral;
+		this.dataNascimento = dataNascimento;
+		this.nomePai = nomePai;
+		this.nomeMae = nomeMae;
+		this.logradouro = logradouro;
+		this.complemento = complemento;
+		this.cep = cep;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getRegistroGeral() {
+		return registroGeral;
+	}
+	public void setRegistroGeral(String registroGeral) {
+		this.registroGeral = registroGeral;
+	}
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public String getNomePai() {
+		return nomePai;
+	}
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+	public String getNomeMae() {
+		return nomeMae;
+	}
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+	public String getLogradouro() {
+		return logradouro;
+	}
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+	public String getComplemento() {
+		return complemento;
+	}
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+}

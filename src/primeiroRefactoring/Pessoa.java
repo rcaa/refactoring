ea5964f42e86bb3634364f5eabac230b603bd149
cefanys <cmb.000@gmail.com>
2022-11-23 20:24:02 -0300

    Refactoring do pacote primeiroRefactoring, Remoção de atributos de endereço da classe Pessoa e criação da Classe Endereço

--- src/primeiroRefactoring/Pessoa.java
+++ src/primeiroRefactoring/Pessoa.java
@@ -10,12 +10,10 @@ public class Pessoa {
 	private Date dataNascimento;
 	private String nomePai;
 	private String nomeMae;
-	private String logradouro;
-	private String complemento;
-	private String cep;
+	private Endereco endereco;
 	
 	public Pessoa(String nome, String cpf, String registroGeral, Date dataNascimento, String nomePai, String nomeMae,
-			String logradouro, String complemento, String cep) {
+			Endereco endereco) {
 		super();
 		this.nome = nome;
 		this.cpf = cpf;
@@ -23,9 +21,8 @@ public class Pessoa {
 		this.dataNascimento = dataNascimento;
 		this.nomePai = nomePai;
 		this.nomeMae = nomeMae;
-		this.logradouro = logradouro;
-		this.complemento = complemento;
-		this.cep = cep;
+		this.endereco = endereco;
+
 	}
 	public String getNome() {
 		return nome;
@@ -63,22 +60,11 @@ public class Pessoa {
 	public void setNomeMae(String nomeMae) {
 		this.nomeMae = nomeMae;
 	}
-	public String getLogradouro() {
-		return logradouro;
+	public Endereco getEndereco() {
+		return endereco;
 	}
-	public void setLogradouro(String logradouro) {
-		this.logradouro = logradouro;
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
 	}
-	public String getComplemento() {
-		return complemento;
-	}
-	public void setComplemento(String complemento) {
-		this.complemento = complemento;
-	}
-	public String getCep() {
-		return cep;
-	}
-	public void setCep(String cep) {
-		this.cep = cep;
-	}
+
 }

c1e9b65c4b8a2022f150c384de3d0f27a9db5d26
Danillo Bion <danillobion@gmail.com>
2022-11-23 22:26:35 -0300

    refatoracao do terceiro pacote

---
+++ src/terceiroRefactoring/Contato.java
@@ -0,0 +1,17 @@
+package terceiroRefactoring;
+
+public class Contato {
+	private String email;
+	
+	public Contato(String email) {
+		this.email = email;
+	}
+	
+	public String  getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
+	
